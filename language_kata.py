d3a27a7151e14e550e5369aa84f59667167e0095
Jeremy Fusco <jeremy.fusco@footclan.org>
2014-07-01 15:58:40 -0700

    Hello World for real

--- language_kata.py
+++ language_kata.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+# This script will serve as a 'kata' for the core language.
+# It is an attempt to incorporate most if not all of the core language into one file.
+
+# As set by PyCharm when creating a new
 __author__ = 'fusco'
+
+
+print("Hello World!")
+
