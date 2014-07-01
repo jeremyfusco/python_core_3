68dcac16599c533ae461ec88ed6ecaa189f61c06
Jeremy Fusco <jeremy.fusco@footclan.org>
2014-07-01 16:05:25 -0700

    Added 'coding:' to language_kata.py
    Also testing a git commit via a call to cygwin git.

--- language_kata.py
+++ language_kata.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-  # utf-8 is the default, but may specify cp-1252 for windows.
 # This script will serve as a 'kata' for the core language.
 # It is an attempt to incorporate most if not all of the core language into one file.
 
